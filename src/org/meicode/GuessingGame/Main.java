3ddfa2844608978f5e83a1c785918002ee958079
Teqwon <59837818+Teqwon@users.noreply.github.com>
2020-11-19 17:14:28 -0500

    initial commit

--- src/org/meicode/GuessingGame/Main.java
+++ src/org/meicode/GuessingGame/Main.java
@@ -4,47 +4,66 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Astro Gaming!");
+        System.out.println("Enter a username:");
+
         Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
-        int numberGuess = rand.nextInt(500) + 1;
-        int remainingTries = 0;
-        int guess;
-        boolean gameEnds = false;
-
-        System.out.println("**************************************************************************************************\n"+
-                "****** Welcome to my infamous guess the number game here you will have 5 tries to guess the ******\n"+
-                "****** number correctly and you will have a little bit of assistance with each number since ******\n"+
-                "****** the range is from numbers 1 - 500, so after the first guess I will notify you if your******\n"+
-                "********** guess was to high or to low to guide you in the right direction so good luck! *********\n" +
-                "**************************************************************************************************\n");
-
-        System.out.println("What is your name? or should I just call you human.\n");
         String name = sc.next();
+        System.out.println("Hello " + name);
 
-        while (!gameEnds){
-            System.out.println("Pick a number 1 - 500: ");
-            guess = sc.nextInt();
-            remainingTries++;
+        System.out.println("Ready to play?");
+        System.out.println("\t1. yes");
+        System.out.println("\t2. no");
 
-            if (guess == numberGuess){
-                gameEnds = true;
-            }
-            else if(guess < numberGuess){
-                System.out.println("Your guess is to low!\n");
+        int start = sc.nextInt();
+
+        while(start != 1){          // if no is selected it will keep displaying the menu and storing the user input
+            System.out.println("Ready to play?");
+            System.out.println("\t1. yes");
+            System.out.println("\t2. no");
+
+            start = sc.nextInt();
+        }
+
+        Random rand = new Random();
+        int numberGuess = rand.nextInt(100) + 1;    //random generator using numbers 1 through 100
+        System.out.println("Take a guess: ");
+        int guess = sc.nextInt();
+
+        int remainingTries = 0;
+        boolean youWin = false;
+        boolean youLose = false;
+
+        while(!youLose){         // this loop will not be broken until you either guess the number correctly or you run
+            remainingTries++;    // run out of tries which will then set
+
+            if(remainingTries < 5){
+                if(guess == numberGuess){
+                    youWin = true;
+                    youLose = true;
+                }
+                else if(guess < numberGuess){
+                    System.out.println("You have to guess higher!");
+                    numberGuess = sc.nextInt();
+                }
+                else {
+                    System.out.println("You have to guess lower!");
+                    numberGuess = sc.nextInt();
+                }
             }
             else {
-                System.out.println("Your guess is to high\n");
+                youLose = true;
             }
         }
 
-        System.out.println("************\n" +
-                "**CONGRATS**\n" +
-                "**YOU WIN **\n" +
-                "************\n");
-        System.out.println("The number was " + numberGuess);
-        System.out.println("It took you " + remainingTries + " tries");
-
-
+        if(youWin == true){
+            System.out.println("Congrats! your a guessing champ! It only took you " + remainingTries + "tries to get it right");
+        }
+        else {
+            System.out.println("Game Over!");
+            System.out.println("The correct number was " + numberGuess);
+        }
     }
 }
